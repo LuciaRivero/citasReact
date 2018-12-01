fc85feeb5fb472f1fbb41da2c5e66dd481f21194
Lucia Rivero <luciarivero@Lucia-Rivero.local>
2018-12-01 15:23:03 -0300

    finalizado add local storage

--- src/App.js
+++ src/App.js
@@ -5,41 +5,72 @@ import ListaCitas from './Componentes/ListaCitas';
 
 class App extends Component {
 
-state = {
-  citas: []
-}
+    state = {
+        citas: []
+    }
 
-crearCita = (nuevaCita) => {
-  //obtener una copia del state
-  const citas = [...this.state.citas, nuevaCita]
-  console.log(citas);
 
-  this.setState({
-    citas
-  });
-}
+    //LOCAL STORAGE
+    componentDidMount() {
+            const citasLS = localStorage.getItem('citas');
+            if (citasLS) {
+                this.setState({
+                    citas: JSON.parse(citasLS) //lo convierto nuvamente en objeto
+                })
+            }
+        }
+        //local storage solo puede almacenar cadena de texto. se almacena segun el dominio,
+    componentDidUpdate() {
+        localStorage.setItem(
+            'citas',
+            JSON.stringify(this.state.citas) //convierte el arreglo en string
 
-  render() {
-    return (
-      <div className="container">
-        <Header
-          titulo={'Turnos pacientes'}
-        />
-        <div className="row">
-          <div className="col-md-6">
-            <AgregarCitas
-              crearCita={this.crearCita}
-            />
-          </div>
-          <div className="col-md-6">
-            <ListaCitas
-              citas = {this.state.citas}
-            />
-          </div>
-        </div>
-      </div>
-    );
-  }
+        )
+    }
+
+    crearCita = (nuevaCita) => {
+        //obtener una copia del state
+        const citas = [...this.state.citas, nuevaCita]
+        console.log(citas);
+
+        this.setState({
+            citas
+        });
+    }
+    borrarCita = (id) => {
+        //objetener citas del state
+        const citasActuales = [...this.state.citas];
+
+        //borrar elemento
+        const citas = citasActuales.filter(cita => cita.id !== id);
+        //actualizar el state
+        this.setState({
+            citas
+        })
+    }
+    render() {
+        return ( <
+            div className = "container" >
+            <
+            Header titulo = { 'Turnos pacientes' }
+            /> <
+            div className = "row" >
+            <
+            div className = "col-md-6" >
+            <
+            AgregarCitas crearCita = { this.crearCita }
+            /> < /
+            div > <
+            div className = "col-md-6" >
+            <
+            ListaCitas citas = { this.state.citas }
+            borrarCita = { this.borrarCita }
+            /> < /
+            div > <
+            /div> < /
+            div >
+        );
+    }
 }
 
 export default App;
--- src/Componentes/ListaCitas.js
+++ src/Componentes/ListaCitas.js
@@ -1,28 +1,31 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import Cita from './Cita';
 
 class ListaCitas extends Component {
-    render() { 
+    render() {
         const citas = this.props.citas;
         const mensaje = Object.keys(citas).length === 0 ? 'No hay citas' : 'Tus citas pendientes: ';
-        return ( 
-            <div className="card mt-5">
-                <div className="card-body">
-                    <h2 className="card-title text-center">
-                        {mensaje}
-                    </h2>
-                    <div className="lista-citas">
-                        {Object.keys(this.props.citas).map(cita => (
-                            <Cita
-                                key={cita}
-                                info={this.props.citas[cita]}
-                            />
-                        ))}
-                    </div>
-                </div>
+        return ( <
+            div className = "card mt-5" >
+            <
+            div className = "card-body" >
+            <
+            h2 className = "card-title text-center" > { mensaje } <
+            /h2> <
+            div className = "lista-citas" > {
+                Object.keys(this.props.citas).map(cita => ( <
+                    Cita key = { cita }
+                    info = { this.props.citas[cita] }
+                    borrarCita = { this.props.borrarCita }
+                    />
+                ))
+            } <
+            /div> <
+            /div>
 
-            </div> );
+            <
+            /div> );
+        }
     }
-}
- 
-export default ListaCitas;
+
+    export default ListaCitas;
